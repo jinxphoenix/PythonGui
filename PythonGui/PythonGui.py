82bfd9ba4fe034639e888524ba6be56304ada8f2
jinxphoenix <thorpete1@gmail.com>
2019-07-06 18:01:09 +0930

    First commit - learnign wxPython using web tutorials

--- PythonGui/PythonGui.py
+++ PythonGui/PythonGui.py
@@ -1,1 +1,11 @@
 
+import wx
+
+class MyFrame(wx.Frame):
+    """ We derive a new class from frame"""
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self,parent,title=title,size=(200,100))
+        self.control = wx.TextCtrl(self,style=wx.TE_MULTiLINE)
+        self.Show(True)
+
+app = wx.App(False)
